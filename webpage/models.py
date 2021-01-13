e025b1c518718b871a13417a2a9ab4388f83e30a
PKSingh0017 <priyanshusingh1998@gmail.com>
2021-01-13 18:46:52 +0530

    added the Member model

--- webpage/models.py
+++ webpage/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+YEAR_CHOICES = [
+    ('1', 'First'),
+    ('2', 'Second'),
+    ('3', 'Third'),
+    ('4', 'Fourth')
+]
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=100, )
+    lastname = models.CharField(max_length=100, )
+    email = models.CharField(max_length=100)
+    year = models.CharField(choices=YEAR_CHOICES, max_length=6)
+    profile_pic = models.ImageField(upload_to='member_profile_pic', default='default.jpg')
+
+    def __str__(self):
+        return self.firstname
