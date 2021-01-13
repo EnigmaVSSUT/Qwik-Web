116a293d4a8179ec2f26652942aefc89c40a3437
parhta2000 <parthaprateem2000@gmail.com>
2021-01-14 02:26:49 +0530

    added a admin buttion

--- cms/urls.py
+++ cms/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin-page'),
     path('accounts/', include('allauth.urls')),
     path('', include('webpage.urls')),
     # path('', include('core.urls')),
