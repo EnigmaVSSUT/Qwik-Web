5feccf995b686b78c8e0a771485b9f8294195bf1
PKSingh0017 <priyanshusingh1998@gmail.com>
2021-01-14 14:14:42 +0530

    added members form

--- cms/settings.py
+++ cms/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.linkedin_oauth2',
-    'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.github',
 ]
 
