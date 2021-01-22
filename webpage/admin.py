09cbf710907f55b9436a0a1b3736524cd44542aa
PKSingh0017 <priyanshusingh1998@gmail.com>
2021-01-22 16:50:32 +0530

    messages styling done, event database added to admin page

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Member, EventRegistration
+from .models import Member, EventRegistration, LiftOffCRegistration
 # Register your models here.
 admin.site.register(Member)
 admin.site.register(EventRegistration)
+admin.site.register(LiftOffCRegistration)
