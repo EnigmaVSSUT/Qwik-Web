263eb178707cbf8e4c9d4eff7087b679da87926e
parhta2000 <parthaprateem2000@gmail.com>
2021-01-25 12:51:37 +0530

    setup model class for questions

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import question_Answer
+
 
 # Register your models here.
+admin.site.register(question_Answer)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+correctChoiceOptions = [('1','1'),('2',2),('3','3'),('4','4')]
+
+class question_Answer(models.Model):
+    quizID   = models.IntegerField(null=False,default=0)
+    question = models.CharField(max_length=200, null=False)
+    option_1 = models.CharField(max_length=100, null=False)
+    option_2 = models.CharField(max_length=100, null=False)
+    option_3 = models.CharField(max_length=100, null=False)
+    option_4 = models.CharField(max_length=100, null=False)
+    corret_choice = models.IntegerField(null=False,default=1,choices = correctChoiceOptions)
+
+    def __str__(self):
+        return self.question
+
