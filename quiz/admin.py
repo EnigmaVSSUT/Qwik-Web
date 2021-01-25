bdd8883de5b90ff4cd39cea5b7bce4112315606d
parhta2000 <parthaprateem2000@gmail.com>
2021-01-25 19:07:06 +0530

    added quiz model to admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import question_Answer
 
+admin.site.site_header = "Enigma Admin Portal"
 
 # Register your models here.
-admin.site.register(question_Answer)
+
+
+class questionData(admin.ModelAdmin):
+    list_display = ('question','option_1','option_2','option_3','option_4','quizID','correct_choice')
+    # list_filter = ('date_of_joining','completely_filled')
+    list_per_page = 20
+    # def complete(self, obj): 
+    #     return obj.completely_filled
+  
+    # complete.boolean = True
+admin.site.register(question_Answer,questionData)
--- quiz/models.py
+++ quiz/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-
 # Create your models here.
 
-correctChoiceOptions = [('1','1'),('2',2),('3','3'),('4','4')]
+correctChoiceOptions = [(1,'1'),(2,'2'),(3,'3'),(4,'4')]
 
 class question_Answer(models.Model):
     quizID   = models.IntegerField(null=False,default=0)
-    question = models.CharField(max_length=200, null=False)
-    option_1 = models.CharField(max_length=100, null=False)
-    option_2 = models.CharField(max_length=100, null=False)
-    option_3 = models.CharField(max_length=100, null=False)
-    option_4 = models.CharField(max_length=100, null=False)
-    corret_choice = models.IntegerField(null=False,default=1,choices = correctChoiceOptions)
+    question = models.TextField()
+    option_1 = models.TextField()
+    option_2 = models.TextField()
+    option_3 = models.TextField()
+    option_4 = models.TextField()
+    correct_choice = models.IntegerField(null=False,default=1,choices =correctChoiceOptions)
 
     def __str__(self):
         return self.question
